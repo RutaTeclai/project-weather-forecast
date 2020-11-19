50f144ed801d5bef62437da4e19d2c2954150a4a
RutaTeclai <rutisyear@gmail.com>
2020-11-19 01:50:39 +0000

    Define functions lat_lng_city_state(city,state) --> lat,lng, get_gridpoints(points) --> forecast_office dict, show_forecast(forecast_url) --> forecast from the API for that url

--- forecast_data.py
+++ forecast_data.py
@@ -8,3 +8,54 @@ import crud
 
 import requests
 import json
+
+
+def lat_lng_city_state(city_name,state):
+
+    city_data = open('data/cities_geodata.json').read()
+
+    city_data_dict = json.loads(city_data)
+    
+    for city in city_data_dict:
+        
+         if city['city'] == city_name and city['state_id'] == state:
+            latitude = city['lat']
+            longitude = city['lng']
+            
+
+    return f'{latitude},{longitude}'
+
+
+def get_gridpoints(points):
+
+    res = requests.get(f'https://api.weather.gov/points/{points}')
+    grid = res.json()
+
+    gridpoints = {'grid_id': grid['properties']['gridId'], 
+                    'grid_x': grid['properties']['gridX'],
+                    'grid_y': grid['properties']['gridY'],
+                    'forecast_hourly': grid['properties']['forecastHourly'],
+                    'forecast': grid['properties']['forecast']
+                }
+    # grid_id = grid['properties']['gridId']
+    # grid_x = grid['properties']['gridX']
+    # grid_y = grid['properties']['gridY']
+
+    return gridpoints
+
+
+def show_forecast(forcast_url):
+
+    # grid_id = gridpoints_dict['grid_id']
+    # grid_x = gridpoints_dict['grid_x']
+    # grid_y = gridpoints_dict['grid_y']
+
+    res = requests.get(f'{forcast_url}')
+    # requests.get(f'https://api.weather.gov/gridpoints/{grid_id}/{grid_x},{grid_y}/forecast')
+
+    forecast = res.json()
+
+    return forecast
+
+
+
